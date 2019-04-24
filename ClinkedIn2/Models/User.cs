82dfb3b61cd9ca17d2fbcf2ae25c4db04e3b5e06
Shane D Wilson <me@shanedwilson.com>
2019-04-23 20:37:25 -0500

    setup

---
+++ ClinkedIn2/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn2.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Age { get; set; }
+        public bool IsPrisoner { get; set; }
+
+
+        public User(int id, string name, int age, bool isPrisoner, DateTime releaseDate)
+        {
+            Id = id;
+            Name = name;
+            ReleaseDate = releaseDate;
+            Age = age;
+            IsPrisoner = isPrisoner;
+        }
+    }
+}
